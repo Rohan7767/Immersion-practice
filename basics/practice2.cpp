d8f1482d5e2139e069831cd368f1be3013391e8c
Rohan7767 <111079640+Rohan7767@users.noreply.github.com>
2023-06-28 13:47:45 +0530

    New Changes made

--- basics/practice2.cpp
+++ basics/practice2.cpp
@@ -1,7 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    float a = 7.0/5;
-    cout<<a<<endl;
+    /*int a = 2;
+    int b = 3;
+    bool first = (a==b);
+    cout<<first<<endl;
+    bool second = (a>b);
+    cout<<second<<endl;
+    bool third = (a<b);
+    cout<<third<<endl;
+    bool fourth = (a>=b);
+    cout<<fourth<<endl;
+    bool fifth = (a<=b);
+    cout<<fifth<<endl;
+    bool sixth = (a!=b);
+    cout<<sixth<<endl;
+    int c = 0;
+    cout<< !c <<endl;*/
+    /*int n = -5;
+    if(n>0){
+        cout<<"n is positive"<<endl;
+    }
+    else{
+        cout<<"n is negative"<<endl;
+    }*/
+    /*int a = 56;
+    int b = 57;
+    if(a>b){
+        cout<<"a>b"<<endl;
+    }
+    else{
+        cout<<"b>a"<<endl;
+    }*/
+    char ch;
+    cout<<"Enter character "<<endl;
+    cin>>ch;
+    if (ch = 'a' - 'z'){
+        cout<<"lowercase"<<endl;
+    }
+    else if (ch = 'A' - 'Z') {
+            cout<<"Uppercase"<<endl;
+    }
+    else{
+        cout<<"numerical"<<endl;
+    }
+
     return 0;
 }
---
+++ pla3.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+void SimpleSieve(int n)
+{
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+  
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            cout << p << " ";
+}
+int main()
+{
+    int n = 100;
+    cout << "Following are the prime numbers from 0 to "<< n << endl;
+    SimpleSieve(n);
+    return 0;
+}
